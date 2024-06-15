fa3e37f897a869b30671bcaa5fe11a12f7c911ae
Docenko Pasha <pashadocenko@gmail.com>
2024-06-15 22:27:02 +0300

    google font was added.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./App.module.scss";
 
 function App() {
-    return <div className={style.check}>hello</div>;
+    return <div className={style.main_page}>hello</div>;
 }
 
 export default App;
