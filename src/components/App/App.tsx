989281c7f7b76e9c8f2500ae01c6c619c0dbc548
Docenko Pasha <pashadocenko@gmail.com>
2024-06-15 22:10:35 +0300

    scss and husku was added.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./App.module.scss"
+
+function App() {
+    return (
+        <div
+                                className={style.check}>
+            hello
+        </div>
+    );
+}
+
+export default App;
