c91a18aaf32438a5a57ca17eea2ae34b973f7ae9
Docenko Pasha <pashadocenko@gmail.com>
2024-06-15 21:51:55 +0300

    eslint and prittier was added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-   <>hello</>
-  );
+    return <>hello</>;
 }
 
 export default App;
