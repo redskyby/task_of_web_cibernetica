084a9bdd0b5c9a736d82b0532abb37343b704626
Docenko Pasha <pashadocenko@gmail.com>
2024-06-16 00:02:34 +0300

    content was added.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import style from "./App.module.scss";
-import row from "../../utils/imgs/row_button.png"
-import left_main_img from "../../utils/imgs/image15.png"
+import row from "../../utils/imgs/row_button.png";
+import left_main_img from "../../utils/imgs/image15.png";
 
 function App() {
-    return (<div className={style.main_page}>
-        <div className={style.left_column}>
-            <div>
-                <h1>Демо-версия</h1>
+    return (
+        <div className={style.main_page}>
+            <div className={style.left_column}>
                 <div>
-                    <p>За 18 лет деятельности было реализовано более 500 крупных, а так же, не менее 2500 средних и малых проектов. Используя знания и накопленный опыт мы можем предложить </p>
+                    <h1>Демо-версия</h1>
+                    <div>
+                        <p>
+                            За 18 лет деятельности было реализовано более 500 крупных, а так же, не менее 2500 средних и
+                            малых проектов. Используя знания и накопленный опыт мы можем предложить{" "}
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <button>Попробовать бесплатно</button>
+                    <img src={row} alt="*" />
                 </div>
             </div>
-            <div>
-                <button>Попробовать бесплатно</button>
-                <img src={row} alt="*"/>
+            <div className={style.right_column}>
+                <div className={style.right_column_image_container}>
+                    <img src={left_main_img} alt="*" />
+                </div>
             </div>
         </div>
-        <div className={style.right_column}>
-            <div className={style.right_column_image_container}>
-            <img src={left_main_img} alt="*"/>
-            </div>
-        </div>
-    </div>);
+    );
 }
 
 export default App;
